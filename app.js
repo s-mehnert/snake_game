7b50e0f6d04bb150808f2586725536d33e8a7a2a
Peafly78 <90190882+Peafly78@users.noreply.github.com>
2024-05-21 20:45:46 +0200

    add html boilerplate, basic CSS styling and JS starting setup

---
+++ app.js
@@ -0,0 +1,54 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const squares = document.querySelectorAll(".grid div");
+    const scoreDisplay = document.querySelector("span");
+    const startBtn = document.querySelector(".start");
+
+    const width = 10;
+    let currentIndex = 0; // first div in grid
+    let appleIndex = 0; // firtst div in grid
+    let currentSnake = [2, 1, 0]; // 2 is the head, 0 is the tail, 1s are the body
+    let direction = 1; // down
+    let score = 0;
+    let speed = 0.9;
+    let intervalTime = 0;
+    let interval = 0;   
+
+
+    // start and restart game
+    function startGame() {
+        currentSnake.forEach(index => squares[index].classList.remove(".snake"));
+        squares[appleIndex].classList.remove("apple");
+        clearInterval(interval);
+        score = 0;
+        // random apple
+        direction = 1;
+        scoreDisplay.innerText = score;
+        intervalTime = 1000;
+        currentSnake = [2, 1, 0];
+        currentIndex = 1;
+        interval = setInterval(moveOutcomes, intervalTime);
+    }
+
+    // dealing with all move outcomes
+    function moveOutcomes() {
+
+    }
+
+    // assign function to keycodes
+    function control(e) {
+        squares[currentIndex].classList.remove("snake");
+        
+        if (e.keyCode === 39) {
+            direction = 1;
+        } else if (e.keyCode === 38) {
+            direction = -width;
+
+        } else if (e.keyCode === 37) {
+            direction = -1;
+        } else if (e.keyCode === 40) {
+            direction = +width;
+        }
+    }
+
+    document.addEventListener("keyup", control);
+});
